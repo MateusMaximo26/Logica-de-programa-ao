76f004f7017b6828afa17ae8ee20641e5e448cce
MateusMaximo26 <53455792+MateusMaximo26@users.noreply.github.com>
2019-11-19 20:52:03 -0300

    Add files via upload

---
+++ posi10_vetor/posi10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace posi10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vetor = new int[10];
+            int[] posicao = new int[6];
+            int i = 0, idade = 0;
+
+            for (i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("Informe a [{0}]º idade do atleta: ");
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("A 6º idade é: " + i);
+
+            for (i = 0; i < vetor.Length; i++)
+            {
+                posicao[i] = vetor[vetor.Length - 4];
+            }
+            Console.WriteLine("[{0}]º = {1}", i, posicao[i]);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ soma_vetor_vetor/soma_vetor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soma_vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[5];
+            int[] B = new int[5];
+            int[] Soma = new int[5];
+            int i;//contador de for
+
+            //inserindo valores no vetor A
+            Console.Write("Inserindo valores no vetor A***\n");
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("Digite um número: ");
+                A[i] = int.Parse(Console.ReadLine());
+            }
+
+            //inserindo valores no vetor B
+            Console.Write("Inserindo valores no vetor B***\n");
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("Digite um número: ");
+                B[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Soma dos vetores
+            Console.Write("Soma dos vetores vetor Soma***\n");
+            Console.WriteLine("Impressão do vetor: ");
+            for (i = 0; i < 5; i++)
+            {
+                Soma[i] = A[i] + B[i];
+                //Os valores são referenciados na memoria Ram como sendo {0},{1}e{2}
+                Console.WriteLine("A[{0}] + B[{1}] = Soma[{2}] ", A[i], B[i], Soma[i]);
+            }
+
+            //Finaliza
+            Console.ReadKey();
+        }
+    }
+}
