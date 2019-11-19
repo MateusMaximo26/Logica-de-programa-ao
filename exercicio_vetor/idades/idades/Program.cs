a1605ba4a6c37995f0594a1f85b98c25eb4b0b33
MateusMaximo26 <53455792+MateusMaximo26@users.noreply.github.com>
2019-11-19 20:46:50 -0300

    Add files via upload

---
+++ exercicio_vetor/idades/idades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Imprimir 5 idades
+
+            int[] idade = new int[5];
+            int i;
+
+            //Inserindo as idade
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("Informe a [{0}º] idade: ", i);
+                idade[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Mostrando as idade
+            Console.WriteLine("As idades são: ");
+            for(i = 0; i < 5; i++)
+            {
+                Console.WriteLine("[{0}º]= {1}", i, idade[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exercicio_vetor/vetor_5num/vetor_5num/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetor_5num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um vetor que leia 5 numeros e imprima o vetor invertido
+            int[] vetor = new int[5];
+            int[] invertido = new int[5];
+            int i;
+
+          
+                Console.Writeline("\n****vetor normal****");
+             
+
+            for (i = 0;i < vetor.Length; i++)
+            {
+                Console.Write("[{0}º] = ", i);
+                vetor[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (i = 0; i < vetor.Length; i++)
+            {
+                invertido[i] = vetor.Length - 1 - i];
+            }
+            Console.WriteLine("\n****vetor normal****");
+            for (i = 0; i < invertido.Length; i++)
+            {
+                Console.WriteLine("[{0}]º = {1}", i, invertido[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
